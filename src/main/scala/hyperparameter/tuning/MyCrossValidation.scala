a2c776a0e3aaa3f1cbb6c31583864905287a9561
Dalei Li <lidalei151781@gmail.com>
2017-01-01 15:02:41 +0100

    Add k-Fold on DataFrame.

--- src/main/scala/hyperparameter/tuning/MyCrossValidation.scala
+++ src/main/scala/hyperparameter/tuning/MyCrossValidation.scala
@@ -3,6 +3,8 @@ package hyperparameter.tuning
 import Helper.InstanceUtilities.Instance
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.storage.StorageLevel
 
 
 
@@ -16,14 +18,16 @@ object MyCrossValidation {
   /**
     * Generate k-fold
     * @param data
-    * @param k, 1 to 100
+    * @param k, 2 to 20
     * @return Array of pair of (RDD[Instance], RDD[Instance]), (train, test)
     */
-  def kFold(data: RDD[Instance], k: Int = 3): Array[(RDD[Instance], RDD[Instance])] = {
-    require(k >= 2 && k <= 100)
+  def kFoldRDD(data: RDD[Instance], k: Int = 3): Array[(RDD[Instance], RDD[Instance])] = {
+    require(k >= 2 && k <= 20, "k should be between 2 and 20, both inclusive.")
 
     // first persist data into memory
-    data.persist()
+    if(data.getStorageLevel == StorageLevel.NONE) {
+      data.persist(StorageLevel.MEMORY_AND_DISK)
+    }
 
     val kFolds: Array[RDD[Instance]] = data.randomSplit(Array.fill[Double](k)(1.0))
 
@@ -32,11 +36,31 @@ object MyCrossValidation {
     val sc: SparkContext = SparkContext.getOrCreate()
 
     kSplits.map((pair: (Array[RDD[Instance]], RDD[Instance])) => (sc.union(pair._1), pair._2))
+  }
 
-//    val numberOfInstances: Long = data.count()
-//    val foldSize: Long = numberOfInstances / k
-//    val kFoldIndices: Array[Long] = (0 until k - 1).toArray.map((i: Int) => i * foldSize) ++ Array(numberOfInstances)
 
+  def kFoldDF(df: DataFrame, k: Int = 3): Array[(DataFrame, DataFrame)] = {
+    require(k >= 2 && k <= 20, "k should be between 2 and 20, both inclusive.")
+
+    // first persist data frame into memory (and disk)
+    df.cache()
+
+    val kFolds: Array[DataFrame] = df.randomSplit(Array.fill[Double](k)(1.0))
+
+    val kSplits = (0 until k).toArray.map((i: Int) => ((0 until k).filter(_ != i).toArray.map(kFolds(_)), kFolds(i)))
+
+    def _union(dfArr: Array[DataFrame]): DataFrame = {
+      require(dfArr.length >= 1, "Union an array of data frames. The length should be at least one.")
+
+      if(dfArr.tail.isEmpty) {
+        dfArr.head
+      }
+      else {
+        dfArr.head.union(_union(dfArr.tail))
+      }
+    }
+
+    kSplits.map((pair: (Array[DataFrame], DataFrame)) => (_union(pair._1), pair._2))
   }
 
 }
