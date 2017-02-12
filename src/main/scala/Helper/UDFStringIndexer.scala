b4323f5a7fbefa4d12b4b1a3ba77c1276dd7f264
Dalei Li <lidalei151781@gmail.com>
2017-02-12 15:14:11 +0100

    Set labels as a parameter.

--- src/main/scala/Helper/UDFStringIndexer.scala
+++ src/main/scala/Helper/UDFStringIndexer.scala
@@ -1,15 +1,32 @@
 package Helper
 
 import org.apache.spark.ml.feature.{StringIndexer, StringIndexerModel}
+import org.apache.spark.ml.param.Param
+import org.apache.spark.ml.util.Identifiable
 import org.apache.spark.sql.Dataset
 
 /**
   * Created by Sophie on 2/11/17.
   */
-class UDFStringIndexer(labels: Array[String]) extends StringIndexer {
+class UDFStringIndexer(uid: String) extends StringIndexer(uid) {
+  def this() = this(Identifiable.randomUID("udfStrIdx"))
+
+  // Treat labels as a parameter. It specifies the labels mapped to indices starting from 0.
+  // For example, Array[String]("cat", "dog", "mouse", ...) maps "cat" to 0, "dog" to 1 and "mouse" to 2, etc.
+  final val labels: Param[Array[String]] = new Param[Array[String]](this, "labels", "user specified labels")
+
+  // Default value is an empty Array. It will invoke builtin fit function.
+  setDefault(labels, Array[String]())
+
+  def setLabels(udfLabels: Array[String]): this.type = set(labels, udfLabels)
 
   override def fit(dataset: Dataset[_]): StringIndexerModel = {
-    transformSchema(dataset.schema, logging = true)
-    copyValues(new StringIndexerModel(uid, labels).setParent(this))
+    if($(labels).isEmpty) {
+      super.fit(dataset)
+    }
+    else {
+      transformSchema(dataset.schema, logging = true)
+      copyValues(new StringIndexerModel($(labels)).setParent(this))
+    }
   }
 }
